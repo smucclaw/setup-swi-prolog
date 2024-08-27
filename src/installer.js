9bb688c7deea2242dbbe8d4c42f635c19e8e14b6
joewatt95 <joewatt95@u.nus.edu>
2024-08-27 09:19:03 +0800

    fix export

--- src/installer.js
+++ src/installer.js
@@ -1,14 +1,12 @@
 import { exec } from '@actions/exec';
 import { join } from 'path';
 
-export default { installSWIProlog };
-
 /**
  * Install SWI-Prolog.
  *
  * @param {string} branch
  * @param {string} version
  */
-async function installSWIProlog(branch, version) {
+export async function installSWIProlog(branch, version) {
   await exec(join(__dirname, 'install-swi-prolog-ubuntu'), [branch, version]);
 }
